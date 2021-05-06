0a049a289ee729ff16a5977a124739f110262938
arexxx <info@arexx.nl>
2021-05-06 17:02:23 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -74,7 +74,7 @@ const hexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c
    // High byte of offStep
    write(chipAddress, pinOffset + channel0OffStepHighByte, (offStep >> 8) & 0x0F)
  }
- 
+
     /**
      * @param lfred, eg: 255
      * @param lfgreen, eg: 255
@@ -86,10 +86,7 @@ const hexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c
         // Add code here
     }
 
-
-
-
-    /**
+    
 
     //% block
     export function setLedDutyCycle(ledNum:number, dutyCycle:number, chipAddress:number): void {
