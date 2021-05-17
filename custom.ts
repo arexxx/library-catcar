da227e0ababdde0d26959b77c00b910e74f7c7b4
arexxx <info@arexx.nl>
2021-05-17 16:12:51 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -150,7 +150,7 @@ namespace CC2 {
     * @param turning kiezen tussen links en rechts draaien
     * @param speed snelheid van de motor in %, eg:0-100
     */
-    //% block="Draai %turning met snelheid %speed ${`%`} "
+    //% block="Draai %turning met snelheid %speed procent"
     export function draaien(turning: Turn = 1, speed: number): void {
       turning = Math.max(1, Math.min(2, turning))
       const pwm_spd = (speed * (chipResolution - 1)) / 100
@@ -177,7 +177,7 @@ namespace CC2 {
     * @param speed snelheid van de motor in %, eg:0-100
     *
     */
-    //% block="rijden %direction met snelheid %speed ${%}"
+    //% block="rijden %direction met snelheid %speed procent"
     export function rijden(direction: Directions = 5, speed: number): void {
       direction = Math.max(5, Math.min(6, direction))
       const pwm_spd = (speed * (chipResolution - 1)) / 100
